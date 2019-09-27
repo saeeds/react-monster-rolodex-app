09afd7139cc0ef530904c93ba5a2ddf6d977f785
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-27 15:24:59 +0300

    Updates

---
+++ precache-manifest.ce9f92b41cacea6164a9a775f54f3a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca826454c021256d45021b0bcad6e57",
+    "url": "/react-monster-rolodex-app/index.html"
+  },
+  {
+    "revision": "73eec47a3e893cb5c5d6",
+    "url": "/react-monster-rolodex-app/static/css/main.5cb42479.chunk.css"
+  },
+  {
+    "revision": "e56ed1d70b373a25b7e5",
+    "url": "/react-monster-rolodex-app/static/js/2.4ba27be9.chunk.js"
+  },
+  {
+    "revision": "73eec47a3e893cb5c5d6",
+    "url": "/react-monster-rolodex-app/static/js/main.7510acee.chunk.js"
+  },
+  {
+    "revision": "b5a11db11d948477c390",
+    "url": "/react-monster-rolodex-app/static/js/runtime-main.74508354.js"
+  }
+]);
