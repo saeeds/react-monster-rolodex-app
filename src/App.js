1ed9c440f31e6e898f5a1a3c5977860afc9d685c
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-27 14:20:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component'
 import './App.css';
 export default class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        }]
+      monsters: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello Leen Alaharqawi' })}>Chnage Text</button>
-        </header>
+        <CardList monsters = {this.state.monsters} />
       </div>
     )
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./card-list.styles.css";
+const CardList = ({ monsters, children }) => {
+  return <div className="card-list"></div>;
+};
+export default CardList;
