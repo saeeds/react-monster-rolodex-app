04d83e2aa46df304a68fdee919869f878587c560
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-27 15:17:24 +0300

    first commit

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./card-list.styles.css";
+import Card from '../card/card.component'
 const CardList = ({ monsters, children }) => {
-  return <div className="card-list"></div>;
+  return (
+    <div className="card-list">
+      {monsters.map(monster => (
+        <Card  key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
 };
 export default CardList;
